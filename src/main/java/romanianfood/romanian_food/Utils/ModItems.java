0749d3c80bd39579810b0bfd1aabc27fa48ab566
Dobrisan Rares Florin <rares.dobrisan@gmail.com>
2025-01-04 16:18:28 +0200

    dada

--- src/main/java/romanianfood/romanian_food/Utils/ModItems.java
+++ src/main/java/romanianfood/romanian_food/Utils/ModItems.java
@@ -1,15 +1,29 @@
 package romanianfood.romanian_food.Utils;
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import net.minecraft.registry.*;
 import net.minecraft.util.Identifier;
 import romanianfood.romanian_food.RomanianFood;
 
+import java.util.function.Function;
+
 public class ModItems {
-    public static Item register(Item item, String id){
-        Identifier ItemID = Identifier.of(RomanianFood.MOD_ID, id);
-        return Registry.register(Registries.ITEM, ItemID, item);
+
+    public static final Item MAMALIGA = ModItems.RegisterItem("mamaliga", Item::new, new Item.Settings());
+
+    public static Item RegisterItem(String name, Function<Item.Settings, Item> factory, Item.Settings settings){
+        final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM,Identifier.of(RomanianFood.MOD_ID,name));
+        return Items.register(registryKey, factory, settings);
     }
-    public static final Item MAMALIGA = register(new Item(new Item.Settings()), "mamaliga");
-    public static void initialize(){}
+    private static void customItems(FabricItemGroupEntries entries){
+        entries.add(MAMALIGA);
+    }
+    public static void initialize(){
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(ModItems::customItems);
+    }
 }
